20b6781b3c2b5592f3f95b9d3be248ecd51d5d25
Lucas_M <59377840+LucasMayhew@users.noreply.github.com>
2022-07-30 18:40:43 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image6":
+            case "myImage":return img`
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c b b b b b b b b c c b b c c 
+c c b b b b b b b b c c b b c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c b b c c b b b b b b b b c c 
+c c b b c c b b b b b b b b c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c b b b b b b b b c c b b c c 
+c c b b b b b b b b c c b b c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+c c c c c c c c c c c c c c c c 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
